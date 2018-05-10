01d283a2ae2160dd6932534e4b88bb81297a80bb
swoopske <32899650+swoopske@users.noreply.github.com>
2018-05-10 11:18:38 +0200

    Added a database project

---
+++ FileHandler2/FileHandler2.Database/dbo/Tables/FileReference.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[FileReference]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[FileUid] UNIQUEIDENTIFIER NOT NULL,
+	[Name] nvarchar(128) NOT NULL,
+	[Url] nvarchar(300) NOT NULL,
+	[Tags] xml NULL,
+	[CDate] datetime NOT NULL DEFAULT GETDATE(),
+	[CBy] varchar(50) NOT NULL DEFAULT suser_sname()
+)
