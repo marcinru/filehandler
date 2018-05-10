3e8dcdb0e9f5f80ecdfb406ec6771ac691a3b5b7
Marcin Rudzki <mar.rudzki@gmail.com>
2018-05-10 15:54:02 +0200

    Add upload file form

--- FileHandler2/ImageResizeWebApp/wwwroot/gallery.js
+++ FileHandler2/ImageResizeWebApp/wwwroot/gallery.js
@@ -6,7 +6,8 @@ new Vue({
     el: '#app',
     data: {
         files: [],
-        search: ''
+        search: '',
+        image: ''
     },
     computed: {
         filteredFiles() {
@@ -29,7 +30,28 @@ new Vue({
             vm.files = res.files;
           }
         })
-    }
+    },
+      methods: {
+        onFileChange(e) {
+          var files = e.target.files || e.dataTransfer.files;
+          if (!files.length)
+            return;
+          this.createImage(files[0]);
+        },
+        createImage(file) {
+          var image = new Image();
+          var reader = new FileReader();
+          var vm = this;
+
+          reader.onload = (e) => {
+            vm.image = e.target.result;
+          };
+          reader.readAsDataURL(file);
+        },
+        removeImage: function (e) {
+          this.image = '';
+        }
+      }
 })
 
 // init gallery for later use
