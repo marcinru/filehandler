dfc0318d11b164344ef805919b9bef6bd83d9a85
coltuszyk <coltuszyk@gmail.com>
2018-05-10 12:12:32 +0200

    Update FileReference.sql

--- FileHandler2/FileHandler2.Database/dbo/Tables/FileReference.sql
+++ FileHandler2/FileHandler2.Database/dbo/Tables/FileReference.sql
@@ -1,10 +1,12 @@
-﻿CREATE TABLE [dbo].[FileReference]
+CREATE TABLE [dbo].[FileReference]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[FileUid] UNIQUEIDENTIFIER NOT NULL,
 	[Name] nvarchar(128) NOT NULL,
 	[Url] nvarchar(300) NOT NULL,
 	[Tags] xml NULL,
+	Actual smallint DEFAULT 1,
+	FileSize int NOT NULL,
 	[CDate] datetime NOT NULL DEFAULT GETDATE(),
 	[CBy] varchar(50) NOT NULL DEFAULT suser_sname()
 )
